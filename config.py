9f680c478144a7f0ad60f2e552fe03f65b003a33
ongzhenghao <77920871+OngZH-00@users.noreply.github.com>
2022-03-17 14:40:39 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cgijukhgzoja.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "12345678"
+customdb = "employee"
+custombucket = "ongzhenghao-employee"
+customregion = "us-east-1"
